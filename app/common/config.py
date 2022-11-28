a606316b08d49604da9bf309d88c7a146d50b5c0
KYG <ygcarrot@gmail.com>
2022-11-28 14:36:47 +0900

    Dozn API 추가

--- app/common/config.py
+++ app/common/config.py
@@ -17,11 +17,30 @@ class Config:
     TRUSTED_HOST = ["*"]
     ALLOW_SITE = ["*"]
 
+    DOZN_URL = "https://test-gw-firm.dozn.co.kr"
+    DOZN_PRIVATE_URL = "https://test-firmapi.dozn.co.kr"
+    DOZN_API_KEY = "bc91572c-77bf-492b-afec-8e8ada809550"
+    DOZN_ORG_CODE = "10000292"
+
 
 @dataclass
 class LocalConfig(Config):
     DB_ECHO: bool = True
     CONSOLE_WRITE: bool = True
+
+
+@dataclass
+class ProdConfig(Config):
+    DOCS_URL: str = None
+    DOZN_URL = "https://firmapi-pub.dozn.co.kr"
+    DOZN_PRIVATE_URL = "https://firmapi.dozn.co.kr"
+    DOZN_API_KEY = "90ab5b89-ecc5-4479-9c19-a3ec0f8388ef"
+    DOZN_ORG_CODE = "10000292"
+
+
+@dataclass
+class DevConfig(Config):
+    DOCS_URL: str = None
 
 
 def conf():
