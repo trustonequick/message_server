954c0a7b3a223c4553412f4bdd519473081be3d0
TaekyuKang <taekyu575@gmail.com>
2022-11-29 15:15:04 +0900

    포트 9000 -> 8000

--- app/main.py
+++ app/main.py
@@ -24,6 +24,6 @@ def create_app():
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 else:
     app = create_app()
